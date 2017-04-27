bb8e86040d7946701aff7921a8db2e1d9ce378e5
Lukey <lshort2@mail.csuchico.edu>
2017-04-27 14:44:46 -0700

    testing gitignore

--- ann.py
+++ ann.py
@@ -17,6 +17,14 @@ class Neuron:
 
 #A good majorify of fxns should just access data[][], as that is where the neurons are stored
 class Ann:
+    """
+    .___       .__  __  .__       .__  .__                __  .__               
+    |   | ____ |__|/  |_|__|____  |  | |__|____________ _/  |_|__| ____   ____  
+    |   |/    \|  \   __\  \__  \ |  | |  \___   /\__  \\   __\  |/  _ \ /    \ 
+    |   |   |  \  ||  | |  |/ __ \|  |_|  |/    /  / __ \|  | |  (  <_> )   |  \
+    |___|___|  /__||__| |__(____  /____/__/_____ \(____  /__| |__|\____/|___|  /
+             \/                 \/              \/     \/                    \/ 
+    """
     def __init__(self):
         self.data = [[] for l in range(NUM_OF_LAYERS)] #where the actual ann's neurons go
         self.m_numIterations =0
@@ -39,8 +47,8 @@ class Ann:
         self.score     = -1#last score achieved by this ANN
         self.highScore = -1#highest score ever achieved by ANN
 
-#This function assigns neurons to elements of the 2d array,
-# ea containing a list of weights initialized to 0.1
+    #This function assigns neurons to elements of the 2d array,
+    # ea containing a list of weights initialized to 0.1
     def constructNetwork(self):
         for i in range( 0, len(self.netStructure) ):# for layer i out of all layers
             tempList = []
@@ -59,12 +67,14 @@ class Ann:
 
             self.data[i] = tempList#add newly created layer to network
 
-    # Run steps 1, 2, and 3. Return direction for pacman based on provided input
-    def processInput(self, listOfInputs):
-        self.step1(listOfInputs)
-        self.step2and3()
-        return self.getDirection()
-
+    """
+     _______          __                       __       _________ __
+     \      \   _____/  |___  _  _____________|  | __  /   _____//  |_  ____ ______  ______
+     /   |   \_/ __ \   __\ \/ \/ /  _ \_  __ \  |/ /  \_____  \\   __\/ __ \\____ \/  ___/
+    /    |    \  ___/|  |  \     (  <_> )  | \/    <   /        \|  | \  ___/|  |_> >___ \
+    \____|__  /\___  >__|   \/\_/ \____/|__|  |__|_ \ /_______  /|__|  \___  >   __/____  >
+            \/     \/                              \/         \/           \/|__|       \/
+    """
     # Iterate through input layer a_j = x_j
     def step1(self, listOfInputs):
         for i in range( 0, len(listOfInputs) ):
@@ -80,16 +90,31 @@ class Ann:
 
                 self.data[l][j].aVal = 1 / (1 + exp(-1 * in_j))
 
-    # Look at output layer and figure out which direction it is saying to go
-    # This is bad and just picks the biggest one. It should do euc dist @@@
-    def getDirection(self):
-        eucDist = 9999
-        goThisWay = Directions.LEFT
-        for j in range( 0, len(self.data[-1]) ): # for ea node in output layer...
-            if self.getEucDist(j) < eucDist:
-                eucDist = self.getEucDist(j)
-                goThisWay = self.directionMapping[j]
-        return goThisWay
+    # Only used for trainig
+    def step4(self):
+        for l in range(1,len(self.data)):# For ea layer 1...L
+            for j in range(0,len(self.data[l]) ):# for ea node j in layer l
+                in_j = 0
+                for neuron in self.data[l-1]:# look a neurons from layer (l-1)
+                    in_j += neuron.aVal * neuron.weights[j]
+
+                self.data[l][j].aVal = 1 / (1 + exp(-1 * in_j))
+
+    """
+       _____ __________.___
+      /  _  \\______   \   |
+     /  /_\  \|     ___/   |
+    /    |    \    |   |   |
+    \____|__  /____|   |___|
+            \/
+    These are function that are called externally or helper functions for
+      functions that are called externally.
+    """
+    # Run steps 1, 2, and 3. Return direction for pacman based on provided input
+    def processInput(self, listOfInputs):
+        self.step1(listOfInputs)
+        self.step2and3()
+        return self.getDirection()
 
     # Calculate Euclidean distance for output vector vs an encoding vector
     # vecNum is a number 0-3 to specify the vector being compared to
@@ -101,6 +126,17 @@ class Ann:
             eucDist += temp
 
         return sqrt(eucDist)
+
+    # Look at output layer and figure out which direction it is saying to go
+    # This is bad and just picks the biggest one. It should do euc dist @@@
+    def getDirection(self):
+        eucDist = 9999
+        goThisWay = Directions.LEFT
+        for j in range( 0, len(self.data[-1]) ): # for ea node in output layer...
+            if self.getEucDist(j) < eucDist:
+                eucDist = self.getEucDist(j)
+                goThisWay = self.directionMapping[j]
+        return goThisWay
 
     # Update score and (sometimes) highscore
     def setScore(self, newScore):
