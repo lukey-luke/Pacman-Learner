37a0684ca875528fc595641ed2cc6862ef41938d
TruHawkins <thawkins7@mail.csuchico.edu>
2017-04-28 16:58:03 -0700

    my name is tru muther fuckers

--- ann.py
+++ ann.py
@@ -2,28 +2,21 @@ import random
 from math import exp
 from math import sqrt
 from game import Directions
-LENGTH_OF_INPUT  = 96
-LENGTH_OF_OUTPUT = 4
-NUM_OF_LAYERS    = 5
-ALPHA_VALUE      = 0.01
+LENGTH_OF_INPUT  = 2 #96
+LENGTH_OF_OUTPUT = 2 #4
+NUM_OF_LAYERS    = 3 #5
 
 class Neuron:
     def __init__(self):
-        self.aVal    = 0 # "a" value - the value this node holds essentially
-        self.delta   = 0 # measure of error for this node
-        self.weights = []# list of weights to all nodes in the next layer
+        self.aVal    = 0
+        #self.delta   = 0
+        self.weights = []
+        #for i in range(0, LENGTH_OF_OUTPUT)):# right when we have a neuron, give it 4 rand wghts
+        #    self.weights.append( random.uniform(0.1, 1.9) ) # @@@
 
 
-#Note to devs: A good majorify of fxns should just access data[][], as that is where the neurons are stored
+#A good majorify of fxns should just access data[][], as that is where the neurons are stored
 class Ann:
-    """
-    .___       .__  __  .__       .__  .__                __  .__               
-    |   | ____ |__|/  |_|__|____  |  | |__|____________ _/  |_|__| ____   ____  
-    |   |/    \|  \   __\  \__  \ |  | |  \___   /\__  \\   __\  |/  _ \ /    \ 
-    |   |   |  \  ||  | |  |/ __ \|  |_|  |/    /  / __ \|  | |  (  <_> )   |  \
-    |___|___|  /__||__| |__(____  /____/__/_____ \(____  /__| |__|\____/|___|  /
-             \/                 \/              \/     \/                    \/ 
-    """
     def __init__(self):
         self.data = [[] for l in range(NUM_OF_LAYERS)] #where the actual ann's neurons go
         self.m_numIterations =0
@@ -41,13 +34,14 @@ class Ann:
                 Directions.SOUTH,
                 Directions.WEST
                 ]
-        self.netStructure = [LENGTH_OF_INPUT, 16, 8, 4, LENGTH_OF_OUTPUT]#solely used to describe structure
+        self.netStructure = [LENGTH_OF_INPUT, 3, LENGTH_OF_OUTPUT]#just used to describe structure
+        #self.netStructure = [LENGTH_OF_INPUT, 16, 8, 4, LENGTH_OF_OUTPUT]#just used to describe structure
         self.constructNetwork()
         self.score     = -1#last score achieved by this ANN
         self.highScore = -1#highest score ever achieved by ANN
 
-    #This function assigns neurons to elements of the 2d array,
-    # ea containing a list of weights initialized to 0.1
+#This function assigns neurons to elements of the 2d array,
+# ea containing a list of weights initialized to 0.1
     def constructNetwork(self):
         for i in range( 0, len(self.netStructure) ):# for layer i out of all layers
             tempList = []
@@ -66,14 +60,12 @@ class Ann:
 
             self.data[i] = tempList#add newly created layer to network
 
-    """
-     _______          __                       __       _________ __
-     \      \   _____/  |___  _  _____________|  | __  /   _____//  |_  ____ ______  ______
-     /   |   \_/ __ \   __\ \/ \/ /  _ \_  __ \  |/ /  \_____  \\   __\/ __ \\____ \/  ___/
-    /    |    \  ___/|  |  \     (  <_> )  | \/    <   /        \|  | \  ___/|  |_> >___ \
-    \____|__  /\___  >__|   \/\_/ \____/|__|  |__|_ \ /_______  /|__|  \___  >   __/____  >
-            \/     \/                              \/         \/           \/|__|       \/
-    """
+    # Run steps 1, 2, and 3. Return direction for pacman based on provided input
+    def processInput(self, listOfInputs):
+        self.step1(listOfInputs)
+        self.step2and3()
+        return self.getDirection()
+
     # Iterate through input layer a_j = x_j
     def step1(self, listOfInputs):
         for i in range( 0, len(listOfInputs) ):
@@ -89,50 +81,16 @@ class Ann:
 
                 self.data[l][j].aVal = 1 / (1 + exp(-1 * in_j))
 
-    # Only used for trainig
-    # Calculate error for ea node in output layer
-    # yVec is the vector of expected outputs - currently a placeholder until we know where this will be stored @@@
-    def step4(self, yVec):
-        for i in range(1,len(self.data[-1])):# For ea node in output layer...
-            data[-1][i].delta = self.data[-1][i] * (1 - self.data[-1][i]) * (yVec[i] - self.data[-1][i])
-
-    # Only used for trainig
-    # Calculate nodes' error for ea layer except the output layer
-    def step5and6(self):
-        for l in range(0, len(self.data)):
-            for n in range(0, len(self.data[l])):
-                newDeltaVal = self.data[l][n].aVal * (1 - self.data[l][n].aVal)
-                for w in range(0, len(self.data[l][n].weights)):
-                    newDeltaVal += (self.data[l][n].weights[w] * self.data[l+1][n].delta)
-
-                self.data[l][n].delta = newDeltaVal
-
-    # Only used for trainig
-    # Recaluclate weights based on error (delta values)
-    def step7(self):
-        for l in range(0, len(self.data)):
-            for n in range(0, self.data[l]):
-                for w in range(0, self.data[l][n].weights):
-                    newWeight = self.data[l][n].weights[w]
-                    newWeight += (ALPHA_VALUE * self.data[l][n].aVal * self.data[l+1][w].delta)
-
-                    self.data[l][n].weights[w] = newWeight
-
-    """
-       _____ __________.___
-      /  _  \\______   \   |
-     /  /_\  \|     ___/   |
-    /    |    \    |   |   |
-    \____|__  /____|   |___|
-            \/
-    These are functions that are called externally or helper functions for
-      functions that are called externally.
-    """
-    # Run steps 1, 2, and 3. Return direction for pacman based on provided input
-    def processInput(self, listOfInputs):
-        self.step1(listOfInputs)
-        self.step2and3()
-        return self.getDirection()
+    # Look at output layer and figure out which direction it is saying to go
+    # This is bad and just picks the biggest one. It should do euc dist @@@
+    def getDirection(self):
+        eucDist = 9999
+        goThisWay = Directions.LEFT
+        for j in range( 0, len(self.data[-1]) ): # for ea node in output layer...
+            if self.getEucDist(j) < eucDist:
+                eucDist = self.getEucDist(j)
+                goThisWay = self.directionMapping[j]
+        return goThisWay
 
     # Calculate Euclidean distance for output vector vs an encoding vector
     # vecNum is a number 0-3 to specify the vector being compared to
@@ -145,21 +103,118 @@ class Ann:
 
         return sqrt(eucDist)
 
-    # Look at output layer and figure out which direction it is saying to go
-    # This is bad and just picks the biggest one. It should do euc dist @@@
-    def getDirection(self):
-        eucDist = 9999
-        goThisWay = Directions.LEFT
-        for j in range( 0, len(self.data[-1]) ): # for ea node in output layer...
-            if self.getEucDist(j) < eucDist:
-                eucDist = self.getEucDist(j)
-                goThisWay = self.directionMapping[j]
-        return goThisWay
-
     # Update score and (sometimes) highscore
     def setScore(self, newScore):
         self.score = newScore
         if newScore > self.highScore:
             self.highScore = newScore
 
+    def giveName(self, n):
+	self.name = n
+
+    def Print(self):
+	print 
+	print self.name, " ", self.highScore
+	nodeCount = 0
+	nextNode = 0
+	hLayNum = 1
+	#going thru layers
+	for i in range(0 , len(self.data)):
+	    print
+	    if i == 0:
+		print "Input Layer"
+	    elif i < len(self.data) - 1:
+		print "Hidden Layer ", hLayNum
+		hLayNum += 1
+	    else:
+		print "Output Layer"
+	    print
+	    nextNode = nodeCount
+	    #going thru Neurons
+	    for k in range(0, len(self.data[i])):
+		nodeCount += 1
+		conIter = 1
+		print "Node ", nodeCount, "-"
+		#going thru Weights
+		for l in range(0, len(self.data[i][k].weights)):
+	            s = "{}".format(self.data[i][k].weights[l])
+		    print "        ----", s, "----> ", nextNode + len(self.data[i]) + conIter
+		    conIter += 1
+
+
+    def PrintLayer(self, num):
+	    nodeCount = 0
+	    for i in range(0, num):
+		nodeCount += len(self.data[i])
+	    nextNode = 0
+	    hLayNum = num
+	    if num < len(self.data):
+	        print
+	        if num == 0:
+		    print "Input Layer"
+	        elif num < len(self.data) - 1:
+		    print "Hidden Layer ", hLayNum
+	        else:
+		    print "Output Layer"
+	        print
+	        nextNode = nodeCount
+	    #going thru Neurons
+	        for k in range(0, len(self.data[num])):
+		    nodeCount += 1
+		    conIter = 1
+		    print "Node ", nodeCount, "-"
+		#going thru Weights
+		    for l in range(0, len(self.data[num][k].weights)):
+	                s = "{}".format(self.data[num][k].weights[l])
+		        print "        ----", s, "----> ", nextNode + len(self.data[num]) + conIter
+		        conIter += 1
+
+
+    def PrintNeuron(self, lNum, nNum):
+	nodeCount = 0
+	for i in range(0, lNum):
+	    nodeCount += len(self.data[i])
+	nodeCount += nNum
+	if lNum < len(self.data):
+	    if nNum < len(self.data[lNum]):
+		conIter = 0
+		print "Node ", nodeCount, "-"
+		#going thru Weights
+		for i in range(0, len(self.data[lNum][nNum].weights)):
+	            s = "{}".format(self.data[lNum][nNum].weights[i])
+		    print "        ----", s, "----> ", nodeCount + len(self.data[lNum]) + conIter
+		    conIter += 1
+
+
+"""
+This is some dummy sample code used to test ANN.
+myObjy = Ann()
+specialArray = [
+        0.1,
+        0.1,
+        0.1,
+        0.1,
+        0.1,
+        0.0,
+        0.1,
+        0.9,
+        0.1,
+        0.1,
+        0.1,
+        0.1,
+        0.1,
+        0.1,
+        0.1,
+        0.9,
+        0.1,
+        0.1,
+        0.1,
+        0.9,
+        0.9,
+        0.1,
+        0.9,
+        0.1
+        ]
+myObjy.processInput(specialArray)
+"""
 
