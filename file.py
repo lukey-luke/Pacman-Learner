75f2f27af2eb829ce8a368ba6cc70110e52daa9e
Chris Martinez <cmartinez77@mail.csuchico.edu>
2017-04-28 22:38:41 -0700

    found solution to print

---
+++ file.py
@@ -0,0 +1,27 @@
+nes = open("won1_input.txt").read().splitlines()
+
+data = []
+for i in range(0, len(nes)):
+    data.append([])
+    for j in range(0, len(nes[0])):
+        data[i].append(int (nes[i][j]))
+
+for i in range(0, len(data)):
+    for j in range(0, len(data[0])):
+        print data[i][j],
+    print '\n'
+
+
+'''
+lines = [line.rstrip('\n') for line in open('won1_labels.txt')]
+labels = []
+for line in lines:
+    line = line.strip().split(' ')
+    labels.append(line)
+data = []
+for line in lines:
+    number_strings = line.split() # Split the line on runs of whitespace
+    numbers = [float(n) for n in number_strings] # Convert to integers
+    data.append(numbers) # Add the "row" to your list.
+print(data) # [[1, 3, 4], [5, 5, 6]]
+'''
